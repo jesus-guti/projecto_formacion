3683a7437c4714e89cdda60f60f99a31af82f6b4
jesus-guti <jesusgutierrezsiliceo@gmail.com>
2024-04-15 18:13:47 +0200

    tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
-
 function App() {
     return (
         <div className="flex">
-            <div className="w-16 navbar">
+            <nav className="w-16 navbar">
                 <img src="logo.png" className="logo" alt="" />
                 <div className="group top-group">
                     <div className="circular-btn">h</div>
-                    <div className="circular-btn-active">f</div>
+                    <div className="circular-btn--active">f</div>
                     <div className="circular-btn">h</div>
                 </div>
                 <div className="group mid-group">
@@ -21,8 +19,29 @@ function App() {
                     <div className="circular-btn">h</div>
                     <div className="circular-btn">h</div>
                 </div>
-            </div>
-            <div className="main w-full">main</div>
+            </nav>
+            <main className="main w-full">
+                <header className="main-header">
+                    <div className="search-container">
+                        <form action="/search" method="get">
+                            <div className="circular-btn">
+                                <img
+                                    src="https://cdn.hugeicons.com/icons/search-01-stroke-rounded.svg"
+                                    alt="search-01"
+                                    width="20"
+                                    height="20"
+                                />
+                            </div>
+                            <input
+                                type="search"
+                                name="q"
+                                className="search-input"
+                                placeholder="Search your today analysis"
+                            />
+                        </form>
+                    </div>
+                </header>
+            </main>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,19 @@ export default {
     theme: {
         extend: {
             colors: {
-                "light-gray": "#F7F7F7",
-                "gray": "#E2E2E2",
-                "dark-gray": "#888888",
-                "darker-gray": "#666666",
-                "main-green": "#5EE85B",
+                "light-gray": "var(--light-gray)",
+                "gray": "var(--gray)",
+                "dark-gray": "var(--darkgray)",
+                "darker-gray": "var(--darker-gray)",
+                "main-green": "var(main-green)",
+            },
+            fontSize: {
+                "xs": "var(--text-xs)",
+                "sm": "var(--text-sm)",
+                "base": "var(--text-base)",
+                "md": "var(--text-md)",
+                "xl": "var(--text-xl)",
+                "2xl": "var(--text-2xl)",
             },
         },
     },
