89a01d2b38c59b13c9e6a1a3ad9874150960a43a
jesus-guti <jesusgutierrezsiliceo@gmail.com>
2024-04-17 19:17:08 +0200

    header and hero section structure

--- src/App.tsx
+++ src/App.tsx
@@ -22,25 +22,90 @@ function App() {
             </nav>
             <main className="main w-full">
                 <header className="main-header">
-                    <div className="search-container">
-                        <form action="/search" method="get">
-                            <div className="circular-btn">
-                                <img
-                                    src="https://cdn.hugeicons.com/icons/search-01-stroke-rounded.svg"
-                                    alt="search-01"
-                                    width="20"
-                                    height="20"
-                                />
-                            </div>
-                            <input
-                                type="search"
-                                name="q"
-                                className="search-input"
-                                placeholder="Search your today analysis"
+                    <form
+                        action="/search"
+                        method="get"
+                        className="rounded-full-container"
+                    >
+                        <div className="circular-btn bg-light-gray">
+                            <img
+                                src="https://cdn.hugeicons.com/icons/search-01-stroke-rounded.svg"
+                                alt="search-01"
+                                width="20"
+                                height="20"
                             />
-                        </form>
-                    </div>
+                        </div>
+                        <input
+                            type="search"
+                            name="q"
+                            className="search-input text-sm"
+                            placeholder="Search your today analysis"
+                        />
+                    </form>
+                    <button className="rounded-full-container text-darker-gray text-sm ml-auto mr-0">
+                        <div className="circular-btn bg-light-gray">
+                            <img
+                                src="https://cdn.hugeicons.com/icons/search-01-stroke-rounded.svg"
+                                alt="search-01"
+                                width="20"
+                                height="20"
+                            />
+                        </div>
+                        <span>Notifications</span>
+                        <div className="medium-badge">
+                            <span>5</span>
+                        </div>
+                    </button>
+                    <div className="avatar">JG</div>
                 </header>
+                <section className="hero">
+                    <h1 className="hero-title">
+                        Your Sales <br />
+                        Analysis
+                    </h1>
+                    <button className="rounded-full-container text-darker-gray text-sm ml-auto mr-0">
+                        <div className="circular-btn bg-light-gray">
+                            <img
+                                src="https://cdn.hugeicons.com/icons/search-01-stroke-rounded.svg"
+                                alt="search-01"
+                                width="20"
+                                height="20"
+                            />
+                        </div>
+                        <span>Notifications</span>
+                        <div className="medium-badge">
+                            <span>5</span>
+                        </div>
+                    </button>
+                    <button className="rounded-full-container text-darker-gray text-sm">
+                        <div className="circular-btn bg-light-gray">
+                            <img
+                                src="https://cdn.hugeicons.com/icons/search-01-stroke-rounded.svg"
+                                alt="search-01"
+                                width="20"
+                                height="20"
+                            />
+                        </div>
+                        <span>Notifications</span>
+                        <div className="medium-badge">
+                            <span>5</span>
+                        </div>
+                    </button>
+                    <button className="rounded-full-container text-darker-gray text-sm">
+                        <div className="circular-btn bg-light-gray">
+                            <img
+                                src="https://cdn.hugeicons.com/icons/search-01-stroke-rounded.svg"
+                                alt="search-01"
+                                width="20"
+                                height="20"
+                            />
+                        </div>
+                        <span>Notifications</span>
+                        <div className="medium-badge">
+                            <span>5</span>
+                        </div>
+                    </button>
+                </section>
             </main>
         </div>
     );
